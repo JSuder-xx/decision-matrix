2f16b9956ff337a08c6d017b2d85d5ac75d02d11
John Suder <j.j.suder@gmail.com>
2020-01-07 19:38:03 -0500

    Fixed .gitignore. Rollup  file casing.

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser';
 
 export default {
-    input: './src/Main.bs.js',
+    input: './src/main.bs.js',
     output: [
         {
             file: './release/main.js',
