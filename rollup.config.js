d3e948be931f8c433985c43ed983a9beb11a2d07
JSuder-xx <j.j.suder@gmail.com>
2020-05-28 12:29:06 -0400

    Moving to /dist to simplify Azure deployments.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import resolve from '@rollup/plugin-node-resolve'
-import {terser} from 'rollup-plugin-terser';
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: './src/main.bs.js',
-    output: [
-        {
-            file: './release/main.js',
-            format: 'iife',
-            name: 'starter',
-            plugins: [terser()]
-        }
-    ],
-    plugins: [resolve()]
-}
+  input: "./src/main.bs.js",
+  output: [
+    {
+      file: "./dist/main.js",
+      format: "iife",
+      name: "starter",
+      plugins: [terser()],
+    },
+  ],
+  plugins: [resolve()],
+};
